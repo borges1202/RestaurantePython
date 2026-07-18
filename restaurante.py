8d4afaa43cdbd5da8ffeb13ad2deeabce44ef5c7
borges1202 <borgess1202@gmail.com>
2026-07-18 09:57:37 -0300

    Adicionei a mudança de estado do status, e arrumei umas coisinhas

--- restaurante.py
+++ restaurante.py
@@ -2,21 +2,31 @@ class Restaurante:
     restaurante_list = []
 
     def __init__(self, name, category):
-        self.name = name
-        self.category = category
-        self.active = False
+        self._name = name.title()
+        self._category = category.title()
+        self._status = False
         Restaurante.restaurante_list.append(self)
 
     def __str__(self):
-        return f'{self.name} / {self.category} / {self.active}'
+        return f'{self._name} | {self._category}'
 
-    def list_restaurantes():
-        for estabelecimentos in Restaurante.restaurante_list:
-            print(estabelecimentos)
+    @classmethod
+    def list_restaurantes(cls):
+        print(f'{'Nome do Restaurante'.ljust(25)} | {'Categoria'.ljust(25)} | {'Status'}')
+        for estabelecimentos in cls.restaurante_list:
+            print(f'{estabelecimentos._name.ljust(25)} | {estabelecimentos._category.ljust(25)} | {estabelecimentos.status}')
+            
+    @property
+    def status(self):
+        return '☑' if self._status else '☐'
+    
+    def switch_state(self):
+        self._status = not self._status
 
 
 rest = Restaurante('Praça', 'Gourmet')
-rest1 = Restaurante('Pizza', 'Fast Food')
+rest1 = Restaurante('Pizza', 'fast food')
 rest2 = Restaurante('habbis', 'Fast food')
 
 Restaurante.list_restaurantes()
+
